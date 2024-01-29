0cbd42b9df80f4c660d5b5333d54d6678a755099
Edgar Méndez <mdzredgar@gmail.com>
2024-01-29 16:11:43 -0600

    Uso de eventos.

--- project/main.py
+++ project/main.py
@@ -5,6 +5,16 @@ app = FastAPI(title='Proyecto para reseñar peliculas',
               version='1')
 
 
+@app.on_event('startup')
+def startup():
+    print('El servidor está iniciando.')
+
+
+@app.on_event('shutdown')
+def shutdown():
+    print('El servidor está finalizando.')
+
+
 @app.get('/')
 async def index():
     return 'Hola mundo desde un servidor en FastAPI'
