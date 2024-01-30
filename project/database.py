1bbca2007c1d1869f4a5cd5dddca9e93610adce4
Edgar Méndez <mdzredgar@gmail.com>
2024-01-30 11:25:25 -0600

    Creación de modelos e implementación al inicio de la aplicación.

--- project/database.py
+++ project/database.py
@@ -1,5 +1,6 @@
 from peewee import *
 from decouple import config
+from datetime import datetime
 
 DATABASE = config('DB_NAME')
 
@@ -8,3 +9,43 @@ database = MySQLDatabase(config('DB_NAME'),
                          password=config('DB_PSWD'),
                          host=config('DB_HOST'),
                          port=config('DB_PORT', cast=int))
+
+
+class User(Model):
+    username = CharField(max_length=50, unique=True)
+    password = CharField(max_length=50)
+    created_at = DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        database = database
+        table_name = 'users'
+
+
+class Movie(Model):
+    title = CharField(max_length=50)
+    created_at = DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        database = database
+        table_name = 'movies'
+
+
+class UserReview(Model):
+    user = ForeignKeyField(User, backref='reviews')
+    movie = ForeignKeyField(Movie, backref='reviews')
+    review = TextField()
+    score = IntegerField()
+    created_at = DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.movie.title} '
+
+    class Meta:
+        database = database
+        table_name = 'user_reviews'
--- project/main.py
+++ project/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from database import database as connection
+from database import User, Movie, UserReview
 
 app = FastAPI(title='Proyecto para reseñar peliculas',
               description='En este proyecto se usara para crear reseñas de peliculas.',
@@ -10,14 +11,14 @@ app = FastAPI(title='Proyecto para reseñar peliculas',
 def startup():
     if connection.is_closed():
         connection.connect()
-        print('Connecting...')
+
+    connection.create_tables([User, Movie, UserReview])
 
 
 @app.on_event('shutdown')
 def shutdown():
     if not connection.is_closed():
         connection.close()
-        print('Close')
 
 
 @app.get('/')
