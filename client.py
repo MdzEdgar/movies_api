ce31ee761bc3ed55df8712744e107d07a53aa2e4
Edgar Méndez <mdzredgar@gmail.com>
2024-02-08 15:09:14 -0600

    Uso de metodo POST con requests

--- client.py
+++ client.py
@@ -1,20 +1,17 @@
 import requests
 
 URL = 'http://localhost:8000/api/v1/reviews'
-HEADERS = {
-    'accept': 'application/json'
-}
-QUERYSET = {
-    'page': 1,
-    'limit': 1
+REVIEW = {
+    'user_id': 1,
+    'movie_id': 1,
+    'review': 'Review creada con request',
+    'score': 3
 }
 
-response = requests.get(URL, headers=HEADERS, params=QUERYSET)
+response = requests.post(URL, json=REVIEW)
 
 if response.status_code == 200:
-    print('Peticion realizada de forma exitosa')
-
-    if response.headers.get('content-type') == 'application/json':
-        reviews = response.json()
-        for review in reviews:
-            print(f"> score: {review['score']} - {review['review']}")
+    print('Reseña creada de forma exitosa.')
+    print(response.json()['id'])
+else:
+    print(response.content)
