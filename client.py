84901ba98bbfb36c5d0002e697bfa5f3465b3f08
Edgar Méndez <mdzredgar@gmail.com>
2024-02-08 14:44:06 -0600

    Envio de parametros.

--- client.py
+++ client.py
@@ -4,8 +4,12 @@ URL = 'http://localhost:8000/api/v1/reviews'
 HEADERS = {
     'accept': 'application/json'
 }
+QUERYSET = {
+    'page': 1,
+    'limit': 1
+}
 
-response = requests.get(URL, headers=HEADERS)
+response = requests.get(URL, headers=HEADERS, params=QUERYSET)
 
 if response.status_code == 200:
     print('Peticion realizada de forma exitosa')
